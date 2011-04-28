5d41869a2d52ef7dff8357efe958ca182c7375d5
Eli Dupuis <elidupuis@gmail.com>
2011-04-27 21:43:46 -0600

    version 0.1

--- jquery.fsbg.js
+++ jquery.fsbg.js
@@ -1,104 +1,107 @@
 /*!
  * jQuery fsbg plugin
- * Copyright (c) 2010 Eli Dupuis
- * Version: 0.2
+ * Copyright (c) Eli Dupuis
+ * Version: 0.1
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) and GPL (http://creativecommons.org/licenses/GPL/2.0/) licenses.
- * Requires: jQuery v1.4.2 or later
- * Based on Fullscreenr by Jan Schneiders (www.nanotux.com)
+ * Requires: jQuery v1.4.2 or later?
+ * Based on Fullscreenr by Jan Schneiders (www.nanotux.com) and Backstrech by Scott Robbin (http://srobbin.com/jquery-plugins/jquery-backstretch/)
  *	TODO:
-    -fade image in. options for different effects?
 		-add option for loading graphic to appear in top left corner? or specified corner.
 		-add slideshow functionality (and next/back functionality)
 		-add option to specify how to handle portrait images. (fill browser or letterbox the image)
-
 */
-(function($) {
 
+(function($) {
+  
   var internals = {
     resize: function(theImg){
       var w = theImg.width(),
-          h = theImg.height();
-
-      // Set bg size
-      var ratio = h / w,
-      // Get browser window size
+          h = theImg.height(),
+          ratio = h / w,
           browserwidth = $(window).width(), 
           browserheight = $(window).height();
+
       // Scale the image
       if ( (browserheight/browserwidth) > ratio ) {
         theImg.height(browserheight).width(browserheight / ratio);
       } else {
         theImg.width(browserwidth).height(browserwidth * ratio);
       }
-      // Center the image
-      theImg.css('left', (browserwidth - theImg.width())/2).css('top', (browserheight - theImg.height())/2);
+      
+      // center the image
+      theImg.css({
+        left: (browserwidth - theImg.width())/2,
+        top: (browserheight - theImg.height())/2
+      });
     }
   };
-
+  
   var methods = {
     init: function( options ) {
-      // iterate and reformat each matched element
-    	return this.each(function() {
-    		var $this = $(this),
-    		    opts = $.extend({}, $.fn.fsbg.defaults, options),
-            data = $this.data('fsbg');
+  		var $body = $('body'),
+  		    opts = $.extend({}, $.fsbg.defaults, options),
+          data = $body.data('fsbg');
 
-        // If the plugin hasn't been initialized yet
-        if ( ! data ) {
-          
-        	var context = $this;
-          
-          if(window.console) window.console.log(context);
-
-        	if (opts.image === null) {
-        		if(window.console) window.console.info("[jquery.fsbg] You must specify an image path in the options to continue.");
-        		return;
-        	};
-
-        	//	wrap body content so we can z-index above image
-        	context.wrapInner($('<div/>').attr({ id: opts.wrapID }));
-
-        	var theImg = $('<img/>', {
+      // If the plugin hasn't been initialized yet
+      if ( ! data ) {
+        
+        if (opts.image === null) {
+      		if(window.console) window.console.info("[jquery.fsbg] You must specify an image path in the options to continue.");
+      		return;
+      	};
+        
+        //	wrap body content so we can z-index above image
+      	$body.wrapInner( $('<div/>', {
+        	  id: opts.wrapID,
+        	  css: {
+        	    position: 'relative',
+        	    zIndex: 2
+        	  }
+        	})
+        );
+        
+        var $fsbg = $('<div/>', {
+      	  id: opts.bgID,
+      	  css:{
+      	    position: 'fixed',
+            zIndex: 1,
+            top: 0,
+            left: 0
+      	  }
+      	}).prependTo($body).append(
+      	  $('<img/>', {
         		src: opts.image,
-        		id: opts.bgID,
+        		css: {
+        		  position: 'absolute'
+        		},
         		load: function(){
-        			$(this).hide().appendTo(context).show(); //fadeIn();
+        			$(this).fadeIn(); //fadeIn();
               internals.resize( $(this) );
         		}
-        	});
+        	}).hide()
+      	);
 
-        	//	event handlers
-          $(window).bind("resize.fsbg", function() {
-           internals.resize( theImg );
-          });
-        	
+      	//	event handlers
+        $(window).bind("resize.fsbg", function() {
+         internals.resize( $fsbg.children('img') );
+        });
+        
 
-          //  attach
-          $(this).data('fsbg', {
-            target: $this,
-            bg: theImg,
-            opts: opts
-          });
+        //  attach
+        $body.data('fsbg', {
+          opts: opts
+        });
 
-        };
-      });
+      };
+      return this;  // For chaining
     },
     update: function() {
-      // each method must loop through all selected elements and return 'this'.
-      return this.each(function() {
-        if(window.console) window.console.log('update called.');
-      });
-    },
-    destroy: function() {
-      // each method must loop through all selected elements and return 'this'.
-      return this.each(function() {
-        if(window.console) window.console.log('destroy called.');
-      });
+      if(window.console) window.console.log('update called.');
     }
   };
 
   // main plugin declaration:
-  $.fn.fsbg = function( method ) {
+  $.fsbg = function( method ) {
     if ( methods[method] ) {
       return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
     } else if ( typeof method === 'object' || ! method ) {
@@ -109,12 +112,10 @@
   };
 
   //	defaults
-  $.fn.fsbg.defaults = {
-  	image: null,
-  	wrapID: 'fullbgBodyWrap',
-    bgID: 'fullbgImg'
+  $.fsbg.defaults = {
+    image: null,
+  	wrapID: 'fsbgBodyWrap',
+    bgID: 'fsbg'
   };
-
-  // $.fn.fsbg.publicfunc = function() { return "jquery.fsbg public function."; };
 
 })(jQuery);
